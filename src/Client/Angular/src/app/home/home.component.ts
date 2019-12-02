34478f967257e39956a55c29d86ee86d7efbd08e
prsad <prsad@cesar.org.br>
2019-12-02 10:35:25 -0200

    adding angular6 part of authentication

---
+++ src/Client/Angular/src/app/home/home.component.ts
@@ -0,0 +1,56 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { AccountService } from '../account.service';
+import { environment } from '../../environments/environment';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit, OnDestroy {
+  isUserAuthenticated = false;
+  subscription: Subscription;
+  userName: string;
+
+  constructor(
+    private httpClient: HttpClient,
+    private accountService: AccountService
+  ) {}
+
+  ngOnInit() {
+    this.subscription = this.accountService.isUserAuthenticated.subscribe(
+      isAuthenticated => {
+        this.isUserAuthenticated = isAuthenticated;
+        if (this.isUserAuthenticated) {
+          this.httpClient
+            .get(`${environment.apiUrl}/test/name`, {
+              responseType: 'text',
+              withCredentials: true
+            })
+            .subscribe(theName => {
+              this.userName = theName;
+            });
+        }
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
+
+  simulateFailedCall() {
+    this.httpClient.get(`${environment.apiUrl}/test/fail`).subscribe();
+  }
+
+  login() {
+    this.accountService.login();
+  }
+}
